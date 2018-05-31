665edc782ac070c635d2e69aae0dffa7fccf1cf5
Sergey Pochooev <wadeoidd@gmail.com>
2018-05-31 16:10:04 +0300

    Данные с текст вьюхи перегнать на вторую тукствьюху

--- app/src/main/java/com/example/wadeo/testapp/MainActivity.java
+++ app/src/main/java/com/example/wadeo/testapp/MainActivity.java
@@ -1,32 +1,38 @@
 package com.example.wadeo.testapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+
+import android.view.View;
 import android.widget.TextView;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 TextView tV1,tV2;
-    Date currentTime = Calendar.getInstance().getTime();
+    public static final String CURRENT_TIME = "CURRENT_TIME";
+SharedPreferences Share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tV1 = findViewById(R.id.tV1);
-        tV1.setText(String.valueOf(currentTime.getTime()));
-        MySharedPreferences.myInstance().SaveData(String.valueOf(currentTime.getTime()));
-        tV2 = findViewById(R.id.tV2);
+        Share = getSharedPreferences(CURRENT_TIME, Context.MODE_PRIVATE);
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        String time = format.format(calendar.getTime());
+        TextView textView = findViewById(R.id.tV2);
+        textView.setText(time);
+
+        tV1 = (TextView) findViewById(R.id.tV1);
+
 
     }
-    private void save(String time) {
-        MySharedPreferences.myInstance().SaveData(time);
-    }
-   /** @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        save(tV1.getText().toString());
-    }**/
+    public void save(View v) {
+        String time  = tV1.getText().toString();SharedPreferences.Editor editor = Share.edit();
+        editor.putString(CURRENT_TIME, time);
+        editor.commit();}
 }
