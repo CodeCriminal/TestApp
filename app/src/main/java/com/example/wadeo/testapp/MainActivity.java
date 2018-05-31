f9a075e6d0ac939a904b43af9ccd3ba8b42c4eda
Sergey Pochooev <wadeoidd@gmail.com>
2018-05-31 16:48:55 +0300

    Данные с текст вьюхи перегнать на вторую тукствьюху

--- app/src/main/java/com/example/wadeo/testapp/MainActivity.java
+++ app/src/main/java/com/example/wadeo/testapp/MainActivity.java
@@ -1,38 +1,43 @@
 package com.example.wadeo.testapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity {
-TextView tV1,tV2;
+public class MainActivity extends Activity {
+    TextView tV1,tV2;
     public static final String CURRENT_TIME = "CURRENT_TIME";
-SharedPreferences Share;
+    SharedPreferences Share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Share = getSharedPreferences(CURRENT_TIME, Context.MODE_PRIVATE);
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
         String time = format.format(calendar.getTime());
         TextView textView = findViewById(R.id.tV2);
         textView.setText(time);
+        MySharedPreferences.myInstance().SaveData(String.valueOf(calendar.getTime()));
+        TextView textView2 = findViewById(R.id.tV1);
+        MySharedPreferences.myInstance().getMyData();
 
-        tV1 = (TextView) findViewById(R.id.tV1);
+        }
 
-
+        private void save(String time) {
+            MySharedPreferences.myInstance().SaveData(time);
+        }
     }
-    public void save(View v) {
-        String time  = tV1.getText().toString();SharedPreferences.Editor editor = Share.edit();
-        editor.putString(CURRENT_TIME, time);
-        editor.commit();}
-}
+
+
+
